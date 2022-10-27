17811c012db6395d40445b923f2abd158386ab50
Matthew S <61678138+mattsegura@users.noreply.github.com>
2022-10-27 14:10:21 -0500

    first commit

---
+++ UILogin.Android/CustomRender/MyEntryRender.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using UILogin.CustomRender;
+using UILogin.Droid.CustomRender;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(MyEntryRenderer))]
+namespace UILogin.Droid.CustomRender
+{
+    public class MyEntryRenderer : EntryRenderer
+    {
+        public MyEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
---
+++ UILogin/CustomRender/CustomEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace UILogin.CustomRender
+{
+    public class CustomEntry : Entry
+    {
+
+    }
+}
